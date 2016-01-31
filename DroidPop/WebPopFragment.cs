ae88580c8a889c70f82f477c91aff48d1ac007d9
hdknr <gmail@hdknr.com>
2016-01-31 11:22:13 +0900

    IOnTouchListener can deltect WebView touch events

--- DroidPop/WebPopFragment.cs
+++ DroidPop/WebPopFragment.cs
@@ -18,7 +18,7 @@ namespace DroidPop
 	/// Web pop fragment.
 	/// http://bit.ly/droid_dialog
 	/// </summary>
-	public class WebPopFragment : DialogFragment
+	public class WebPopFragment : DialogFragment, View.IOnTouchListener
 	{
 		public override void OnCreate (Bundle savedInstanceState)
 		{
@@ -44,13 +44,21 @@ namespace DroidPop
 			var webview = view.WebView (Resource.Id.WebContent);
 			webview.LoadData (@"<html><body><a href=""#close"">Close</a></body></html>",
 				"text/html", "utf-u");
-			
-			view.WebView(Resource.Id.WebContent).Click += (object sender, EventArgs e) => {
-				Android.Util.Log.Debug("WebPopFragment", "Web View Clicked");
-			};
+			webview.SetOnTouchListener (this);
+//			webview.Click += (object sender, EventArgs e) => {
+//				Android.Util.Log.Debug("WebPopFragment", "Web View Clicked");
+//			};
 			return view;
 		
 		}
+
+		public bool OnTouch(View v, MotionEvent e)
+		{
+			Android.Util.Log.Debug ("WebPopFragment", "OnTouch {0}", e.Action.ToString ());
+			return true;
+		}
+			
 	}
+
 }
 
